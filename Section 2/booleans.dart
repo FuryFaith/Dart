75e48a59029d2013a7ee4cdacc57b01cd1d3387f
aravindhakshan0880 <aravind.000.as@gmail.com>
2021-02-04 10:16:18 +0530

    Boolean part

---
+++ Section 2/booleans.dart
@@ -0,0 +1,12 @@
+void main() {
+  bool ison; //This is a variable
+  print(ison);
+
+  bool isDog = false; //varible declaration with value\
+  print(isDog);
+
+  ison = true;
+  print('isOn = ${ison}');
+
+  print('ison is a ${ison.runtimeType}');
+}
